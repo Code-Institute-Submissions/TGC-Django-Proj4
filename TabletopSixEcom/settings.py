65f44c9197de84d0c5fcd7e2f6fa9d693ddb9aa0
CollinWuY <collinwu.y@gmail.com>
2020-12-27 05:01:02 +0000

    Updated settings.py

--- TabletopSixEcom/settings.py
+++ TabletopSixEcom/settings.py
@@ -141,7 +141,22 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# simulate Django to send out emails
+TEST_EMAIL = os.environ.get("TEST_EMAIL")
+if TEST_EMAIL == "1":
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
